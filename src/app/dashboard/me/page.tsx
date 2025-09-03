7a0f7c0f139d833183bdf4f12dcd7443272a9deb
Davy Lopes <125506032+DavyL0@users.noreply.github.com>
2025-09-02 21:39:56 -0300

    Refactor dashboard to use UrlPreview component
    Replaces the hardcoded URL preview in the dashboard 'me' page with the UrlPreview component for improved modularity and maintainability.

--- src/app/dashboard/me/page.tsx
+++ src/app/dashboard/me/page.tsx
@@ -1,5 +1,6 @@
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
+import { UrlPreview } from "./_components/url";
 
 export default async function Me() {
 
@@ -19,17 +20,9 @@ export default async function Me() {
     
   return (
     <main className="w-full h-full flex gap-4 flex-col items-center p-4">
-      <section
-        className="w-full flex lg:flex-row flex-col lg:items-center mx-auto bg-zinc-900 rounded-md p-4 gap-2"
-      >
-        <p
-          className="w-fit h-9 rounded-md flex items-center font-semibold text-white"
-        >
-          http://minha_url.com/creator/fulano-dev
-        </p>
+      <section className="w-full flex lg:flex-row flex-col lg:items-center mx-auto bg-zinc-900 rounded-md p-4 gap-2">
+       <UrlPreview></UrlPreview>
       </section>
-
-
     </main >
   )
 }
